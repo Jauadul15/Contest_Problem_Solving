ea4c68cdcb19fafb76eac4189e4cadbfb8a38004
Jauadul15 <rashik.gmh@gmail.com>
2025-07-28 19:51:42 +0600

    Find minimum value and its index in an array

--- problem5.c
+++ problem5.c
@@ -4,22 +4,21 @@ int main(){
     int n;
     scanf("%d",&n);
     int a[n];
-    int min=INT_MAX;
-    int pos = 0;
+    int min = INT_MAX;
+    int position = 0;
     for (int i = 0; i < n; i++)
     {
         scanf("%d",&a[i]);
     }
-    
     for (int i = 0; i < n; i++)
     {
-        if (a[i]<min)
+        if (a[i] < min)
         {
-            min=a[i];
-            pos =i;
+            min = a[i];
+            position = i;
         }
         
     }
-    printf("%d %d",min,pos);
+    printf("%d %d",min,position);
     
 }
